f970aa97e19810bd29794dbc12f5c5bc9f503ae0
nanco <nanco_pu@163.com>
2021-04-25 15:31:13 +0800

    优化：google chrome 使用

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -2,5 +2,9 @@
 ### every exit != 0 fails the script
 set -e
 
+# 依赖
+sudo apt-get install -y dialog
+
 echo "Install Chrome Browser"
 sudo apt install -y -f $HOME/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i $HOME/google-chrome-stable_current_amd64.deb
